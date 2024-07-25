beee08ab65f7bfc80790e5e4e66ca3c6e3d17053
NB Matlala <njabulo_matlala@yahoo.com>
2024-07-25 09:00:25 +0200

    Added new Logic App trigger.py

--- PrivatePropDev.py
+++ PrivatePropDev.py
@@ -278,7 +278,7 @@ async def main2():
         print(f"Inside File uploaded to Azure Blob Storage: {blob_name}")
 
     body = ''
-    requests.post('https://prod2-09.southafricanorth.logic.azure.com:443/workflows/623f55b4346742178048b209a003f895/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=PxhThWSLOC3sS3JAg54Z3uZEhTvU9zAbNIhkaAhMPN0', json=body)        
+    requests.post('https://prod2-31.southafricanorth.logic.azure.com:443/workflows/85956207b15d4806af09d4ed0d78a404/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=9wSTRAla28G_81SoFVz14lbacRE14PJMRmjjKpo3v_I', json=body)        
 
 
 
