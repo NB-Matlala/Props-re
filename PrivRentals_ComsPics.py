8f10315d99dd362667e8bc51500c2ef4ef1c422e
NB Matlala <njabulo_matlala@yahoo.com>
2026-03-31 10:35:45 +0200

    Update PrivRentals_ComsPics.py

--- PrivRentals_ComsPics.py
+++ PrivRentals_ComsPics.py
@@ -25,214 +25,216 @@ con_str = os.getenv("CON_STR")
 con_str_coms = os.getenv("CON_STR_COMS")
 log_trg = os.getenv("LOG_TRG")
 
-def getIds(soup):
-    try:
-        url = soup['href']
+print(log_trg)
 
-        prop_ID_match = re.search(r'/([^/]+)$', url)
-        if prop_ID_match:
-            return prop_ID_match.group(1)
-    except Exception as e:
-        print(f"Error extracting ID from {soup}: {e}")
-    return None
+# def getIds(soup):
+#     try:
+#         url = soup['href']
 
-def getPages(soupPage, url):
-    try:
-        num_pg = soupPage.find('div', class_='sort-and-listing-count')
-        # ('div', class_='listing-results-layout__mobile-item-count txt-small-regular')
-        num_pgV = num_pg.text.split('of ')[-1]
-        num_pgV = num_pgV.replace('\xa0', '').replace(' results', '')
-        pages = math.ceil(int(num_pgV) / 20)
-        return pages
-    except (ValueError, AttributeError) as e:
-        print(f"Failed to parse number of pages for URL: {url} - {e}")
-        return 0
+#         prop_ID_match = re.search(r'/([^/]+)$', url)
+#         if prop_ID_match:
+#             return prop_ID_match.group(1)
+#     except Exception as e:
+#         print(f"Error extracting ID from {soup}: {e}")
+#     return None
+
+# def getPages(soupPage, url):
+#     try:
+#         num_pg = soupPage.find('div', class_='sort-and-listing-count')
+#         # ('div', class_='listing-results-layout__mobile-item-count txt-small-regular')
+#         num_pgV = num_pg.text.split('of ')[-1]
+#         num_pgV = num_pgV.replace('\xa0', '').replace(' results', '')
+#         pages = math.ceil(int(num_pgV) / 20)
+#         return pages
+#     except (ValueError, AttributeError) as e:
+#         print(f"Failed to parse number of pages for URL: {url} - {e}")
+#         return 0
     
 
-prop_types = ['houses-to-rent', 'apartments-to-rent', 'townhouses-to-rent','land-to-rent', 'garden-cottages-to-rent']
-# prop_types = ['land-to-rent', 'garden-cottages-to-rent']
+# prop_types = ['houses-to-rent', 'apartments-to-rent', 'townhouses-to-rent','land-to-rent', 'garden-cottages-to-rent']
+# # prop_types = ['land-to-rent', 'garden-cottages-to-rent']
 
 
-for pt in prop_types:
-    x = f"{base_url}/{pt}/south-africa/1?"
-    try:
-        land = session.get(x)
-        land_html = BeautifulSoup(land.content, 'html.parser')
-        pgs = getPages(land_html, x)
+# for pt in prop_types:
+#     x = f"{base_url}/{pt}/south-africa/1?"
+#     try:
+#         land = session.get(x)
+#         land_html = BeautifulSoup(land.content, 'html.parser')
+#         pgs = getPages(land_html, x)
 
-        for p in range(1, pgs + 1):
-            url = f"{x}page={p}"
-            pg_request = session.get(url)
-            pg_html = BeautifulSoup(pg_request.content, 'html.parser')
-            ###
-            prop_contain = pg_html.find_all('a', class_='featured-listing')
-            prop_contain.extend(pg_html.find_all('a', class_='listing-result'))
-            for x_page in prop_contain:
-                prop_id = getIds(x_page)
-                if prop_id:
-                    list_ids.append(prop_id)
+#         for p in range(1, pgs + 1):
+#             url = f"{x}page={p}"
+#             pg_request = session.get(url)
+#             pg_html = BeautifulSoup(pg_request.content, 'html.parser')
+#             ###
+#             prop_contain = pg_html.find_all('a', class_='featured-listing')
+#             prop_contain.extend(pg_html.find_all('a', class_='listing-result'))
+#             for x_page in prop_contain:
+#                 prop_id = getIds(x_page)
+#                 if prop_id:
+#                     list_ids.append(prop_id)
     
-    except Exception as e:
-        print(f"Failed to process URL {x}: {e}")
+#     except Exception as e:
+#         print(f"Failed to process URL {x}: {e}")
 
 
-# Thread worker function
-def worker(queue, results, pic_results):
-    while True:
-        item = queue.get()
-        if item is None:
-            break
-        url = item.get("url")
-        extract_function = item.get("extract_function")
-        try:
-            response = session.get(url)
-            soup = BeautifulSoup(response.content, 'html.parser')
+# # Thread worker function
+# def worker(queue, results, pic_results):
+#     while True:
+#         item = queue.get()
+#         if item is None:
+#             break
+#         url = item.get("url")
+#         extract_function = item.get("extract_function")
+#         try:
+#             response = session.get(url)
+#             soup = BeautifulSoup(response.content, 'html.parser')
 
-            print(f"[Response Status: {response.status_code}]\n")
+#             print(f"[Response Status: {response.status_code}]\n")
 
-            result = extract_function(soup, url)
-            if result:
-                if extract_function == extractor_com:
-                    results.append(result)
-                elif extract_function == extractor_pics:
-                    pic_results.extend(result)
-        except Exception as e:
-            print(f"Request failed for {url}: {e}")
-        finally:
-            queue.task_done()
+#             result = extract_function(soup, url)
+#             if result:
+#                 if extract_function == extractor_com:
+#                     results.append(result)
+#                 elif extract_function == extractor_pics:
+#                     pic_results.extend(result)
+#         except Exception as e:
+#             print(f"Request failed for {url}: {e}")
+#         finally:
+#             queue.task_done()
 
-def extractor_com(soup, url):
-    try:
-        prop_ID = None
-        prop_div = soup.find('div', class_='property-details')
-        lists = prop_div.find('ul', class_='property-details__list')
-        features = lists.find_all('li')
-        for feature in features:
-            icon = feature.find('svg').find('use').get('href')
-            if '#listing-alt' in icon:
-                prop_ID = feature.find('span', class_='property-details__value').text.strip()
-    except KeyError:
-        prop_ID = None
+# def extractor_com(soup, url):
+#     try:
+#         prop_ID = None
+#         prop_div = soup.find('div', class_='property-details')
+#         lists = prop_div.find('ul', class_='property-details__list')
+#         features = lists.find_all('li')
+#         for feature in features:
+#             icon = feature.find('svg').find('use').get('href')
+#             if '#listing-alt' in icon:
+#                 prop_ID = feature.find('span', class_='property-details__value').text.strip()
+#     except KeyError:
+#         prop_ID = None
     
-    prop_desc = None
-    latitude = None
-    longitude = None
+#     prop_desc = None
+#     latitude = None
+#     longitude = None
     
-    try:
-        comment_div = soup.find('div', class_='listing-description__text')
-        prop_desc = comment_div.text.strip()
-    except:
-        print('Error. Cannot find comments')
+#     try:
+#         comment_div = soup.find('div', class_='listing-description__text')
+#         prop_desc = comment_div.text.strip()
+#     except:
+#         print('Error. Cannot find comments')
     
-    current_datetime = datetime.now().strftime('%Y-%m-%d')
+#     current_datetime = datetime.now().strftime('%Y-%m-%d')
     
-    return {"Listing ID": prop_ID, "Description": prop_desc, "Latitude": latitude, "Longitude": longitude,"Time_stamp": current_datetime}
+#     return {"Listing ID": prop_ID, "Description": prop_desc, "Latitude": latitude, "Longitude": longitude,"Time_stamp": current_datetime}
 
 
 
-def extractor_pics(soup, prop_ID): # extracts from created urls
-    try:
-        prop_ID = None
-        prop_div = soup.find('div', class_='property-details')
-        lists = prop_div.find('ul', class_='property-details__list')
-        features = lists.find_all('li')
-        for feature in features:
-            icon = feature.find('svg').find('use').get('href')
-            if '#listing-alt' in icon:
-                prop_ID = feature.find('span', class_='property-details__value').text.strip()
-    except KeyError:
-        prop_ID = None
-    list_id = prop_ID
+# def extractor_pics(soup, prop_ID): # extracts from created urls
+#     try:
+#         prop_ID = None
+#         prop_div = soup.find('div', class_='property-details')
+#         lists = prop_div.find('ul', class_='property-details__list')
+#         features = lists.find_all('li')
+#         for feature in features:
+#             icon = feature.find('svg').find('use').get('href')
+#             if '#listing-alt' in icon:
+#                 prop_ID = feature.find('span', class_='property-details__value').text.strip()
+#     except KeyError:
+#         prop_ID = None
+#     list_id = prop_ID
     
-    try:
-        photo_div = soup.find('div', class_='details-page-photogrid__photos')
-        photo_data = []
-        img_links = photo_div.find_all('img')
-        count = 0
-        for url in img_links:
-            count += 1
-            photo_data.append({'Listing_ID': list_id, 'Photo_Link': url.get('src')})
-            if count == 8:
-                break
-        return photo_data        
-    except KeyError:
-        print('Pictures not found')
-        return []
+#     try:
+#         photo_div = soup.find('div', class_='details-page-photogrid__photos')
+#         photo_data = []
+#         img_links = photo_div.find_all('img')
+#         count = 0
+#         for url in img_links:
+#             count += 1
+#             photo_data.append({'Listing_ID': list_id, 'Photo_Link': url.get('src')})
+#             if count == 8:
+#                 break
+#         return photo_data        
+#     except KeyError:
+#         print('Pictures not found')
+#         return []
 
-fieldnames = ['Listing ID', 'Description', 'Latitude', 'Longitude', 'Time_stamp']
-filename = "PrivRentComments.csv.gz"
+# fieldnames = ['Listing ID', 'Description', 'Latitude', 'Longitude', 'Time_stamp']
+# filename = "PrivRentComments.csv.gz"
 
-fieldnames_pics = ['Listing_ID', 'Photo_Link']
-filename_pics = "PrivRentPictures.csv.gz"
+# fieldnames_pics = ['Listing_ID', 'Photo_Link']
+# filename_pics = "PrivRentPictures.csv.gz"
 
-# Initialize thread queue and results list
-queue = Queue()
-results = []
-pic_results = []
+# # Initialize thread queue and results list
+# queue = Queue()
+# results = []
+# pic_results = []
 
   
-for list in list_ids:
-    try:
-        list_url = f"{base_url}//to-rent/something/something/something/{list}"
-        queue.put({"url": list_url, "extract_function": extractor_com})
-        queue.put({"url": list_url, "extract_function": extractor_pics})
-    except Exception as e:
-        print(f"Failed to process URL {list_url}: {e}")
+# for list in list_ids:
+#     try:
+#         list_url = f"{base_url}//to-rent/something/something/something/{list}"
+#         queue.put({"url": list_url, "extract_function": extractor_com})
+#         queue.put({"url": list_url, "extract_function": extractor_pics})
+#     except Exception as e:
+#         print(f"Failed to process URL {list_url}: {e}")
 
-# Start threads
-num_threads = 10  
-threads = []
-for i in range(num_threads):
-    t = threading.Thread(target=worker, args=(queue, results, pic_results))
-    t.start()
-    threads.append(t)
+# # Start threads
+# num_threads = 10  
+# threads = []
+# for i in range(num_threads):
+#     t = threading.Thread(target=worker, args=(queue, results, pic_results))
+#     t.start()
+#     threads.append(t)
 
-# Block until all tasks are done
-queue.join()
+# # Block until all tasks are done
+# queue.join()
 
-# Stop workers
-for i in range(num_threads):
-    queue.put(None)
-for t in threads:
-    t.join()
+# # Stop workers
+# for i in range(num_threads):
+#     queue.put(None)
+# for t in threads:
+#     t.join()
 
-# Write results to CSV files
-gz_filename = filename
+# # Write results to CSV files
+# gz_filename = filename
 
-with gzip.open(gz_filename, "wt", newline="", encoding="utf-8") as gzfile:
-    fieldnames = results[0].keys() if results else []
-    writer = csv.DictWriter(gzfile, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(results)
+# with gzip.open(gz_filename, "wt", newline="", encoding="utf-8") as gzfile:
+#     fieldnames = results[0].keys() if results else []
+#     writer = csv.DictWriter(gzfile, fieldnames=fieldnames)
+#     writer.writeheader()
+#     writer.writerows(results)
 
-gz_filename_pics = filename_pics
-with gzip.open(gz_filename_pics, "wt", newline="", encoding="utf-8") as gzfile:
-    writer = csv.DictWriter(gzfile, fieldnames=fieldnames_pics)
-    writer.writeheader()
-    writer.writerows(pic_results)
+# gz_filename_pics = filename_pics
+# with gzip.open(gz_filename_pics, "wt", newline="", encoding="utf-8") as gzfile:
+#     writer = csv.DictWriter(gzfile, fieldnames=fieldnames_pics)
+#     writer.writeheader()
+#     writer.writerows(pic_results)
 
-## Upload to Azure Blob Storage
-blob_connection_string = f"{con_str_coms}"
-blob = BlobClient.from_connection_string(
-    blob_connection_string,
-    container_name="comments-pics",
-    blob_name=gz_filename
-)
-with open(gz_filename, "rb") as data:
-    blob.upload_blob(data, overwrite=True)
+# ## Upload to Azure Blob Storage
+# blob_connection_string = f"{con_str_coms}"
+# blob = BlobClient.from_connection_string(
+#     blob_connection_string,
+#     container_name="comments-pics",
+#     blob_name=gz_filename
+# )
+# with open(gz_filename, "rb") as data:
+#     blob.upload_blob(data, overwrite=True)
 
-blob_pics = BlobClient.from_connection_string(
-    blob_connection_string,
-    container_name="comments-pics",
-    blob_name=gz_filename_pics
-)
-with open(gz_filename_pics, "rb") as data:
-    blob_pics.upload_blob(data, overwrite=True)
+# blob_pics = BlobClient.from_connection_string(
+#     blob_connection_string,
+#     container_name="comments-pics",
+#     blob_name=gz_filename_pics
+# )
+# with open(gz_filename_pics, "rb") as data:
+#     blob_pics.upload_blob(data, overwrite=True)
 
 
 ###########################################################################################
 import requests
 
 body = {"Pipeline" : 3}
-requests.post(f'{log_trg}', json=body)
+requests.post(log_trg, json=body)
 print("Request sent.")
